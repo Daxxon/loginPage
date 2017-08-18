779d6c30fd8fab784551ca3f8959c524e4420408
Daniel Waggoner <daxxon37@hotmail.com>
2017-08-17 22:13:57 -0400

    turning in assignment

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const mustacheExpress = require('mustache-express');
+const expressValidator = require('express-validator');
 
 const app = express();
+
 const morgan = require("morgan");
+
 let users = [
   {username: 'daxxon', password: 'bojangles'},
   {username: 'guest', password: 'password'},
@@ -19,6 +22,8 @@ app.set('view engine', 'mustache');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(expressValidator());
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -30,13 +35,7 @@ app.use(session({
 app.use(express.static(__dirname));
 
 app.use((req, res, next) => {
-  console.log("Your session ID: " + req.session.id);
-  console.log("Requesting root URL: " + (req.originalUrl === '/'));
-  console.log("There is a user: " + (typeof req.session.username !== 'undefined'));
-  console.log("User: " + req.session.user);
-  console.log("OriginalUrl with NO user: " + ((req.originalUrl === '/') && (typeof req.session.username === 'undefined')));
-  console.log(req.session);
-  console.log(req.body);
+  // console.log(req.session);
   if (req.originalUrl === '/' && typeof req.session.username === 'undefined') {
     console.log("REDIRECTING TO LOGIN PAGE");
     res.redirect('/login');
@@ -46,11 +45,10 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send(`
-    <title>TITLE</title>
-    <span>I AM A SPAN</span>
-    <span>I AM NOT A P</span>
-    `);
+  let myUser = {};
+  myUser.username = req.session.username;
+  myUser.password = req.session.password;
+  res.render('index', myUser);
 });
 
 app.get('/login/', (req, res) => {
@@ -58,9 +56,21 @@ app.get('/login/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  req.session.username = req.body.username;
-  console.log("LOGGING IN...");
-  res.redirect('/');
+  let myUsername = req.body.username;
+  let myPassword = req.body.password;
+  // let myUser;
+
+  users.find((user) => {
+    if (user.username === myUsername && user.password === myPassword) {
+      req.session.username = myUsername;
+      req.session.password = myPassword;
+      // myUser = users.find((user) => {
+      //   return user.username === myUsername;
+      // });
+      console.log("REDIRECTING TO THE MAIN PAGE");
+    }
+  });
+res.redirect('/');
 });
 
 app.listen(3000, () => console.log('SHOW ME WHAT YOU GOT'));
@@ -68,3 +78,11 @@ app.listen(3000, () => console.log('SHOW ME WHAT YOU GOT'));
 /*
 128.64.32.16.8.4.2.1
 */
+
+// console.log(users.find((user) => {
+//   return user.username === myUsername;
+// }));
+//
+// console.log(users.find((user) => {
+//   return user.password === myPassword;
+// }));
